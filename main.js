049d439ad7da04e52e77321898ce77c0a61c0507
marshall5368 <136056077+marshall5368@users.noreply.github.com>
2024-10-08 01:23:07 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+var dropDown = document.getElementById("dropDown")
+var hidden = document.getElementById("hidden")
+function Menu(){
+    if(dropDown.style.display == "flex"){
+        dropDown.style.display = "none";
+        
+    }else{
+        dropDown.style.display = "flex"
+    }
+}
+
+//Starrr//
+
+
+var star1 = document.getElementById("Star1")
+var star2 = document.getElementById("Star2")
+var star3 = document.getElementById("Star3")
+var star4 = document.getElementById("Star4")
+var star5 = document.getElementById("Star5")
+
+function Star1(){
+    if(star1.style.color == "yellow"){
+        star1.style.color = "var(--secondary)"
+    }else{
+        star1.style.color = "yellow"
+    }
+}
+
+function Star2(){
+    if(star2.style.color == "yellow"){
+        star2.style.color = "var(--secondary)"
+    }else{
+        star2.style.color = "yellow"
+    }
+}
+
+function Star3(){
+    if(star3.style.color == "yellow"){
+        star3.style.color = "var(--secondary)"
+    }else{
+        star3.style.color = "yellow"
+    }
+}
+
+function Star4(){
+    if(star4.style.color == "yellow"){
+        star4.style.color = "var(--secondary)"
+    }else{
+        star4.style.color = "yellow"
+    }
+}
+
+function Star5(){
+    if(star5.style.color == "yellow"){
+        star5.style.color = "var(--secondary)"
+    }else{
+        star5.style.color = "yellow"
+    }
+}
